85caa68a6e3129dbfb340e678b629970a813feed
Christopher Garrett <me@pzuraq.com>
2018-07-08 10:36:13 -0700

    [WIP] Initial Implementation

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 'use strict';
 
+function isProductionEnv() {
+  const isProd = /production/.test(process.env.EMBER_ENV);
+  const isTest = process.env.EMBER_CLI_TEST_COMMAND;
+
+  return isProd && !isTest;
+}
+
 module.exports = {
-  name: 'ember-core-object-polyfill'
+  name: 'ember-core-object-polyfill',
+
+  postprocessTree(type, tree) {
+    if (type === 'all') {
+      let PolyfillFilter = require('./lib/polyfill');
+      return new PolyfillFilter(tree, isProductionEnv() ? 'prod' : 'debug');
+    }
+
+    return tree;
+  }
 };
---
+++ tests/unit/polyfill-test.js
@@ -0,0 +1,106 @@
+import { module, test } from 'ember-qunit';
+import EmberObject from '@ember/object';
+
+module('polyfill', function() {
+  module('native classes', function() {
+    test('it works with class fields', function(assert) {
+      class Foo extends EmberObject {
+        first = 'rob';
+        last = 'jackson';
+      }
+
+      let foo = Foo.create({ first: 'tom', last: 'dale' });
+
+      assert.equal(foo.first, 'tom');
+      assert.equal(foo.last, 'dale');
+    });
+
+    test('init is after constructor', function(assert) {
+      let calls = [];
+
+      class Foo extends EmberObject {
+        constructor() {
+          calls.push('foo before constructor');
+          super(...arguments);
+          calls.push('foo after constructor');
+        }
+
+        init() {
+          calls.push('foo before init');
+          super.init(...arguments);
+          calls.push('foo after init');
+        }
+      }
+
+      class Bar extends Foo {
+        constructor() {
+          calls.push('bar before constructor');
+          super(...arguments);
+          calls.push('bar after constructor');
+        }
+
+        init() {
+          calls.push('bar before init');
+          super.init(...arguments);
+          calls.push('bar after init');
+        }
+      }
+
+      Bar.create();
+
+      assert.deepEqual(calls, [
+        'bar before constructor',
+        'foo before constructor',
+        'foo after constructor',
+        'bar after constructor',
+        'bar before init',
+        'foo before init',
+        'foo after init',
+        'bar after init',
+      ]);
+    });
+  });
+
+  module('ember object model', function() {
+    test('it works with class properties', function(assert) {
+      const Foo = EmberObject.extend({
+        first: 'rob',
+        last: 'jackson',
+      });
+
+      let foo = Foo.create({ first: 'tom', last: 'dale' });
+
+      assert.equal(foo.first, 'tom');
+      assert.equal(foo.last, 'dale');
+    });
+  });
+
+  test('init still works', function(assert) {
+    let calls = [];
+
+    const Foo = EmberObject.extend({
+      init() {
+        calls.push('foo before init');
+        super.init(...arguments);
+        calls.push('foo after init');
+      },
+    });
+
+    const Bar = Foo.extend({
+      init() {
+        calls.push('bar before init');
+        super.init(...arguments);
+        calls.push('bar after init');
+      },
+    });
+
+    Bar.create();
+
+    assert.deepEqual(calls, [
+      'bar before init',
+      'foo before init',
+      'foo after init',
+      'bar after init',
+    ]);
+  });
+});
